171bbbce6b3abdd44cf69075184d0d2313b5a89b
Callum Lewis <callumlewis852@gmail.com>
2024-07-12 17:52:36 +0100

    Adds easter egg through JS

---
+++ CheckTheConsole.js
@@ -0,0 +1,5 @@
+console.log("Hello Inquisitive person who decided to check my source code, Feel free to browse.")
+console.log("At the moment of writing this code I have just started to learn JS.")
+console.log("I have decided to make this easter egg for my first project.")
+
+
